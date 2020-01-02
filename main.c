f44a02e0a154cdc6383dc0ae0972121c23ae7a26
AlGol86 <59099639+AlGol86@users.noreply.github.com>
2020-01-02 21:14:35 +0300

    Update main.c

--- main.c
+++ main.c
@@ -24,18 +24,27 @@ char step_AMP=0;
 int T1=0;
 int T2=0;
 char state_indicator=0; //0-ampers 100-t1(time in load) 200-t2(interval on-on) 
-//////////////////////////1-30    -ampers for 30 days 
-//////////////////////////100-130 -time in load for 30 days
-//////////////////////////200-230 -interval on-on in load for 30 days
+//////////////////////////1-30 ampers for 30 days 
+//////////////////////////100-130 time in load for 30 days
+//////////////////////////200-230 interval on-on in load for 30 days
 char timer_flags=0;
 char counti=0;
 int sys_del=0;
 
-
 int main( void )
 { 
+for (char i=0;i<10;i++) y[i]=0;
+for (char i=0;i<10;i++) day_T1[i]=0;
+for (char i=0;i<10;i++) day_T2[i]=0;
+for (char i=0;i<10;i++) day_AMP[i]=0;
+for (char i=0;i<30;i++) month_T1[i]=0;
+for (char i=0;i<30;i++) month_T2[i]=0;
+for (char i=0;i<30;i++) month_AMP[i]=0;
+for (char i=0;i<30;i++) count_t_o[i]=0;
+
 init_buttons();
 init_tim1();
+  
  while(1)
  {  
  indicator(y); 
